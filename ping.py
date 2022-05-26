6e7ac7b68134038884a00020fa00ba613d0f05f0
leonidpeskov1337 <98895117+leonidpeskov1337@users.noreply.github.com>
2022-05-26 12:15:12 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,7 +2,7 @@ import requests
 
 def check_server_status(url):
     a = requests.get(url)
-    if a.status_code  = 200:
+    if a.status_code  == 200:
         return True
     else:
         return False
